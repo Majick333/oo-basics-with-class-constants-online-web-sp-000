a21e6c8d9da008675196e2daad2097557d607213
Ryan M <rmccauleymusic@gmail.com>
2019-11-06 03:17:52 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,14 +2,16 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def brand=(brand)
     @brand = brand
     BRANDS << brand
   end
 
-  
 
-  BRANDS = []
+
+
 
   def cobble
     self.condition = "new"
